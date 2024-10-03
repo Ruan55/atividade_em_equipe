c8de897e2d57db9168b90a6f1bc69b8d261317a4
Roberto <161033388+Hubierto@users.noreply.github.com>
2024-10-03 14:41:16 +0000

    alterado

--- arquivo2_.py
+++ arquivo2_.py
@@ -2,5 +2,24 @@ print("Robeto")
 
 
 
+
 print("continuando projeto.")
+
+
+print("continuando projeto.")
+
+# Resolução de conflito
+#git rebase
+
+#Apague as linhas indesejadas
+
+#git add.
+
+#git commit -m "mensagem"
+
+#git rebase --continue
+
+#git pull
+
+#git push
 
